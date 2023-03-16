e9ddc4e236ad5b1cc05d615a5385492b2a760099
kmeixner247 <konstantin.meixner@freenet.de>
2023-03-16 17:04:50 +0100

    yo sully works :D

---
+++ Sully/Sully.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+char*s="#include <stdlib.h>%c#include <stdio.h>%cchar*s=%c%s%c;%cint main(){%cint i=%d;%cchar fn[11];%csnprintf(fn,11,%cSully_%cd.c%c,i-1);%cFILE*f=fopen(fn,%cw%c);%cfprintf(f,s,10,10,34,s,34,10,10,i-1,10,10,34,37,34,10,34,34,10,10,10,10,34,37,37,37,34,10);%cfclose(f);%cchar cmd[500];%csnprintf(cmd,500,%cgcc -o Sully_%cd %cs && ./Sully_%cd%c,i-1,fn,i-1);%cif(i>1)system(cmd);}";
+int main(){
+int i=5;
+char fn[11];
+snprintf(fn,11,"Sully_%d.c",i-1);
+FILE*f=fopen(fn,"w");
+fprintf(f,s,10,10,34,s,34,10,10,i-1,10,10,34,37,34,10,34,34,10,10,10,10,34,37,37,37,34,10);
+fclose(f);
+char cmd[500];
+snprintf(cmd,500,"gcc -o Sully_%d %s && ./Sully_%d",i-1,fn,i-1);
+if(i>1)system(cmd);}
