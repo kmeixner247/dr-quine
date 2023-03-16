62bfcc6973fd1ac36965938c6d9ac8d9b8300a0d
kmeixner247 <konstantin.meixner@freenet.de>
2023-03-16 14:31:05 +0100

    comment and main adjusted to restriction

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-//macros in macros..
-#define STR char*s="#include <stdio.h>%c%c%cmacros in macros..%c#define STR char*s=%c%s%c;%c#define CODE FILE*f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,s,10,47,47,10,34,s,34,10,34,34,34,34,10,10);fclose(f);%c#define MAIN int main(){STR CODE}%cMAIN";
-#define CODE FILE*f=fopen("Grace_kid.c","w");fprintf(f,s,10,47,47,10,34,s,34,10,34,34,34,34,10,10);fclose(f);
-#define MAIN int main(){STR CODE}
-MAIN
+/*
+    macros in macros..
+*/
+#define STR char*s="#include <stdio.h>%c%c*%c    macros in macros..%c*%c%c#define STR char*s=%c%s%c;%c#define CODE FILE*f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,s,10,47,10,10,47,10,34,s,34,10,34,34,34,34,10,10);fclose(f);%c#define MAIN() int main(){STR CODE}%cMAIN()";
+#define CODE FILE*f=fopen("Grace_kid.c","w");fprintf(f,s,10,47,10,10,47,10,34,s,34,10,34,34,34,34,10,10);fclose(f);
+#define MAIN() int main(){STR CODE}
+MAIN()
