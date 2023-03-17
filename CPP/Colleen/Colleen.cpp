5e0c131ee05923e89b6fed1a81b1095bc1fd2c73
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-03-17 22:35:25 +0100

    CPP Colleen without printf just because I can

---
+++ CPP/Colleen/Colleen.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+string s[]={
+"#include <iostream>",
+"using namespace std;",
+"string s[]={",
+"};",
+"char q=34;",
+"void print_from_until(string s[], int f, int u, bool m) {",
+"    for (int i=f;i<=u;i++) {",
+"        if (m) cout << q;",
+"        cout << s[i];",
+"        if (m) cout << q << ',';",
+"        cout << endl;",
+"    }",
+"    /*",
+"        remember the C quine with write I mentioned?",
+"    */",
+"}",
+"int main() {",
+"    print_from_until(s,0,2,false);",
+"    print_from_until(s,0,23,true);",
+"    print_from_until(s,3,23,false);",
+"}",
+"/*",
+"    this is roughly how it would have worked",
+"*/",
+};
+char q=34;
+void print_from_until(string s[], int f, int u, bool m) {
+    for (int i=f;i<=u;i++) {
+        if (m) cout << q;
+        cout << s[i];
+        if (m) cout << q << ',';
+        cout << endl;
+    }
+    /*
+        remember the C quine with write I mentioned?
+    */
+}
+int main() {
+    print_from_until(s,0,2,false);
+    print_from_until(s,0,23,true);
+    print_from_until(s,3,23,false);
+}
+/*
+    this is roughly how it would have worked
+*/
