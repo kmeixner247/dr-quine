efdd49ba7b7d3e25ab4811374d88651e45630059
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-03-18 14:01:46 +0100

    moved comment to main

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -3,9 +3,10 @@
     I tried to make this work without printf
 */
 void do_it(){
+char*s="#include <stdio.h>%c%c*%c    I tried to make this work without printf%c*%c%cvoid do_it(){%cchar*s=%c%s%c;%cprintf(s,10,47,10,10,47,10,10,34,s,34,10,10,10,47,10,10,47,10);}%cint main(){%c%c*%c    and almost had it, but it was just too long%c*%c%cdo_it();return 0;}";
+printf(s,10,47,10,10,47,10,10,34,s,34,10,10,10,47,10,10,47,10);}
+int main(){
 /*
     and almost had it, but it was just too long
 */
-char*s="#include <stdio.h>%c%c*%c    I tried to make this work without printf%c*%c%cvoid do_it(){%c%c*%c    and almost had it, but it was just too long%c*%c%cchar*s=%c%s%c;%cprintf(s,10,47,10,10,47,10,10,47,10,10,47,10,34,s,34,10,10);}%cint main(){do_it();return 0;}";
-printf(s,10,47,10,10,47,10,10,47,10,10,47,10,34,s,34,10,10);}
-int main(){do_it();return 0;}
+do_it();return 0;}
